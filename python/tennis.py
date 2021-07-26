87e3cb61c572a95f87c9792c5b0cbc27b67fb4ab
Aniruddha B <aniruddhabarapatre@gmail.com>
2021-07-26 21:14:35 +0530

    Breaking down code logic into individual methods

--- python/tennis.py
+++ python/tennis.py
@@ -13,39 +13,53 @@ class TennisGame1:
             self.p1points += 1
         else:
             self.p2points += 1
-    
-    def score(self):
-        result = ""
-        tempScore=0
-        if (self.p1points==self.p2points):
-            result = {
+
+    def equal_points(self, points):
+        result = {
                 0 : "Love-All",
                 1 : "Fifteen-All",
                 2 : "Thirty-All",
-            }.get(self.p1points, "Deuce")
-        elif (self.p1points>=4 or self.p2points>=4):
-            minusResult = self.p1points-self.p2points
-            if (minusResult==1):
-                result ="Advantage " + self.player1Name
-            elif (minusResult ==-1):
-                result ="Advantage " + self.player2Name
-            elif (minusResult>=2):
-                result = "Win for " + self.player1Name
+            }.get(points, "Deuce")
+        return result
+
+    def unequal_points(self):
+        result = ""
+        for i in range(1,3):
+            if ( i == 1 ):
+                tempScore = self.p1points
             else:
-                result ="Win for " + self.player2Name
+                result += "-"
+                tempScore = self.p2points
+            result += {
+                0 : "Love",
+                1 : "Fifteen",
+                2 : "Thirty",
+                3 : "Forty",
+            }[tempScore]
+        return result
+
+    def decider_point(self):
+        result = ""
+        minusResult = self.p1points - self.p2points
+        if ( minusResult == 1 ):
+            result = "Advantage " + self.player1Name
+        elif ( minusResult == -1 ):
+            result = "Advantage " + self.player2Name
+        elif (minusResult >= 2):
+            result = "Win for " + self.player1Name
         else:
-            for i in range(1,3):
-                if (i==1):
-                    tempScore = self.p1points
-                else:
-                    result+="-"
-                    tempScore = self.p2points
-                result += {
-                    0 : "Love",
-                    1 : "Fifteen",
-                    2 : "Thirty",
-                    3 : "Forty",
-                }[tempScore]
+            result = "Win for " + self.player2Name
+        return result
+    
+    def score(self):
+        result = ""
+        tempScore = 0
+        if ( self.p1points == self.p2points ):
+            result = self.equal_points(self.p1points)
+        elif ( self.p1points >= 4 or self.p2points >= 4 ):
+            result = self.decider_point()
+        else:
+            result = self.unequal_points()
         return result
 
 
